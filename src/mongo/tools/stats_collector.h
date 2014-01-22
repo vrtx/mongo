f7ba6882e094c9aa6a06353c83542f6b7e2cea84
Ben Becker <ben.becker@mongodb.com>
2014-01-22 08:56:19 -0800

    fix msgid tracking bug when running multiple shells.  add report of queries which exceeded the given threshold

--- src/mongo/tools/stats_collector.h
+++ src/mongo/tools/stats_collector.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdio.h>
+#include <cmath>
+#include <algorithm>
 
 #include "mongo/base/initializer.h"
 #include "mongo/base/string_data.h"
@@ -33,7 +35,7 @@ public:
       case mongo::opReply:
         if (m.header()->responseTo.get() > 0 && m.header()->getCursor() == 0)
           // log when reply indicates results have been exhausted
-          logResponse(m.header()->responseTo.get());
+          logResponse(m.header()->responseTo.get(), host, port);
         break;
       case mongo::dbQuery:
         // log start time
@@ -43,7 +45,7 @@ public:
         break;
       case mongo::dbKillCursors:
         // mark the query as finished
-        logResponse(m.header()->responseTo.get());
+        logResponse(m.header()->responseTo.get(), host, port);
         break;
       default:
         // unhandled operation
@@ -78,6 +80,44 @@ public:
         break;
       }
     }
+
+    // generate a summary of queries outside of the accepted range
+    if (fprintf(reportFile, "==== Queries with delta exceeding threshold: ====\n") < 0) {
+      printf("error writing to file: %s\n", strerror(errno));
+    }
+    mongo::string lastHost;
+    mongo::StringData lastHash;
+    int lastPort = 0;
+    int32_t lastTotalTime = 0;
+    uint32_t lastCount = 0;
+    int32_t totalOverThreshold = 0;
+    for (ResultsMap::const_iterator i = aggResults.begin(); i != aggResults.end(); ++i) {
+      mongo::StringData prefix = i->first.substr(0, i->first.find(" || ("));
+      if (lastHash.startsWith(prefix)) {
+        // last query matches current query
+        if (std::abs(lastTotalTime - i->second.totalTime) > std::min(lastTotalTime, i->second.totalTime) * 0.03 &&
+            std::abs(lastTotalTime - i->second.totalTime) >= 2) {
+          // delta is > 3% (and at least 2 microseconds)
+          ++totalOverThreshold;
+          if (fprintf(reportFile, "q1 time/count: %d/%d\t q2 time/count: %d/%d\t query: %s\n",
+                                  lastTotalTime, lastCount,
+                                  i->second.totalTime, i->second.count,
+                                  i->first.rawData()) < 0) {
+            printf("error writing to file: %s\n", strerror(errno));
+            break;
+          }
+        }
+      }
+      lastHash = prefix;
+      lastCount = i->second.count;
+      lastTotalTime = i->second.totalTime;
+      lastHost = i->second.host;
+      lastPort = i->second.port;
+    }
+
+    if (fprintf(reportFile, "\n ==\n Number of queries over threshold: %d / %lu\n", totalOverThreshold, aggResults.size() / 2)) {
+      printf("error writing to file: %s\n", strerror(errno));
+    }
     fflush(reportFile);
   }
 
@@ -89,14 +129,18 @@ public:
 private:
   struct QueryStat {
     mongo::string signature;
+    mongo::string host;
+    int port;
     uint64_t startTime;
     uint64_t totalTime;
   };
   struct AggResults {
-    uint32_t totalTime;
+    int32_t totalTime;
     uint32_t count;
+    mongo::string host;
+    int port;
   };
-  typedef mongo::unordered_map<int32_t, QueryStat> QueryMap;
+  typedef mongo::unordered_map<mongo::string, QueryStat> QueryMap;
   // typedef mongo::unordered_map<mongo::StringData, AggResults, mongo::StringData::Hasher> ResultsMap;
   typedef mongo::map<mongo::StringData, AggResults> ResultsMap;
   QueryMap queryStats;
@@ -114,17 +158,24 @@ private:
     dbm.pullInt();  // skip nToSkip
     dbm.pullInt();  // skip nToRetrun
     mongo::stringstream s;
-    s << dbm.nextJsObj().toString(false, true) << " (" << host << ":" << port << ")";
-    stat.signature = s.str();
+    s << dbm.nextJsObj().toString(false, true);
     while (dbm.moreJSObjs())
-      stat.signature += "\t" + dbm.nextJsObj().toString(false, true);
-
-    queryStats[m.header()->id.get()] = stat;
+      s << "\t" + dbm.nextJsObj().toString(false, true);
+    s << " || (" << host << ":" << port << ")";
+    stat.host = host;
+    stat.port = port;
+    stat.signature = s.str();
+    queryStats[generateUniqueMessageId(m.header()->id.get(), host, port)] = stat;
+  }
+  mongo::string generateUniqueMessageId(int32_t msgid, char* host, int port) {
+    mongo::stringstream s;
+    s << host << port << msgid;
+    return s.str();
   }
 
-  void logResponse(int32_t msgid) {
+  void logResponse(int32_t msgid, char* host, int port) {
     uint64_t now = mongo::curTimeMicros64();
-    QueryStat &stat = queryStats[msgid];
+    QueryStat &stat = queryStats[generateUniqueMessageId(msgid, host, port)];
     stat.totalTime = now - stat.startTime;
   }
 
